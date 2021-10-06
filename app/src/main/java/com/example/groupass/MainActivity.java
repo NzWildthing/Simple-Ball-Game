962acaa042921192ef5367bfc839f310ea9e1d6e
NzWildthing <brogan131001@gmail.com>
2021-10-06 19:29:13 +1300

    Implemented Intents with Activity navigation

--- app/src/main/java/com/example/groupass/MainActivity.java
+++ app/src/main/java/com/example/groupass/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.groupass;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    //When the start game button is clicked the game activity is opened
+    public void onClickStart(View v){
+        //Creates an explicit intent which opens up the GameActivity class
+        Intent start = new Intent(this, GameActivity.class);
+        startActivity(start);
+    }
+
+    //When the high scores button is clicked the high score list is opened
+    public void onClickScore(View v){
+        //Creates an explicit intent which opens up the HighScores class
+        Intent open = new Intent(this, HighScores.class);
+        startActivity(open);
     }
 }
